dd9c0927e816bf57434d66d4c52d835d5acbffdf
Ryan Anderson <rustyoldrake@gmail.com>
2018-03-13 00:35:00 -0700

    Beyond Verbal Python basic code to hit API with WAV files

---
+++ BVC.py
@@ -0,0 +1,22 @@
+import requests
+requests.packages.urllib3.disable_warnings()
+
+def getAnalysis(API_Key,WavPath):
+    res = requests.post("https://token.beyondverbal.com/token",data={"grant_type":"client_credentials","apiKey":API_Key})
+    token = res.json()['access_token']
+    headers={"Authorization":"Bearer "+token}
+    pp = requests.post("https://apiv3.beyondverbal.com/v3/recording/start",json={"dataFormat": { "type":"WAV" }},verify=False,headers=headers)
+    recordingId = pp.json()['recordingId']
+    with open(WavPath,'rb') as wavdata:
+        r = requests.post("https://apiv3.beyondverbal.com/v3/recording/"+recordingId,data=wavdata, verify=False, headers=headers)
+        return r.json()
+
+
+json = getAnalysis("API_KEY_GOES_HERE","audio_test_boredsad.wav")
+print(json)
+
+    # * Sadness/Uncertainty/Boredom
+    # * Anger/Dislike/Stress
+    # * Neutral
+    # * Happiness/Enthusiasm/Friendliness
+    # * Warmth/Calmness
